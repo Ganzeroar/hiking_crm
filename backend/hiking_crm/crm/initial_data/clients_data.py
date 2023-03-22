bfc518e723632b6adcbabc7130167b64f424154b
Dmitriy <ganzeroar@gmail.com>
2023-03-22 19:39:17 +0600

    change phones in clients initial data

--- backend/hiking_crm/crm/initial_data/clients_data.py
+++ backend/hiking_crm/crm/initial_data/clients_data.py
@@ -1,39 +1,39 @@
 first_client_name = 'Соловьева Арина Максимовна'
 first_client_email = 'queitreubrotrobru-8373@yopmail.com'
-first_client_phone = '+74(87)743-06-82'
+first_client_phone = '+74877430682'
 
 second_client_name = 'Колесов Кирилл Артёмович'
 second_client_email = 'catroxedizeu-9688@yopmail.com'
-second_client_phone = '+6(690)237-15-18'
+second_client_phone = '+79669023715'
 
 third_client_name = 'Кудряшова Варвара Всеволодовна'
 third_client_email = 'gaudafufroyau-1158@yopmail.com'
-third_client_phone = '+56(2001)982-29-80'
+third_client_phone = '+79256200198'
 
 fourth_client_name = 'Фирсов Артём Степанович'
 fourth_client_email = 'meuvexalibe-7943@yopmail.com'
-fourth_client_phone = '+1(6766)904-06-59'
+fourth_client_phone = '+79266690406'
 
 fifth_client_name = 'Егоров Лев Алексеевич'
 fifth_client_email = 'quoutennasuta-7667@yopmail.com'
-fifth_client_phone = '+1(035)530-45-23'
+fifth_client_phone = '+79725530452'
 
 sixth_client_name = 'Гончаров Владимир Павлович'
 sixth_client_email = 'prayoujeummama-8281@yopmail.com'
-sixth_client_phone = '+36(033)906-04-76'
+sixth_client_phone = '+79736339060'
 
 seventh_client_name = 'Васильева Мия Ивановна'
 seventh_client_email = 'freinneuppoyollu-6769@yopmail.com'
-seventh_client_phone = '+6(1275)802-94-71'
+seventh_client_phone = '+61275802947'
 
 eighth_client_name = 'Яковлева Арина Юрьевна'
 eighth_client_email = 'kogreuffauhogru-6851@yopmail.com'
-eighth_client_phone = '+255(6823)208-78-47'
+eighth_client_phone = '+79255682320'
 
 ninth_client_name = 'Соколова Виктория Михайловна'
 ninth_client_email = 'hiwerousage-5079@yopmail.com'
-ninth_client_phone = '+19(9457)760-58-87'
+ninth_client_phone = '+79994577605'
 
 tenth_client_name = 'Нестерова Александра Александровна'
 tenth_client_email = 'jojeubomata-1776@yopmail.com'
-tenth_client_phone = '+45(38)104-08-25'
+tenth_client_phone = '+79824538104'
