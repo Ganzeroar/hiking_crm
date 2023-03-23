60360550e2b04f185ef0c247a999849b78f29512
Dmitriy <ganzeroar@gmail.com>
2023-03-23 17:17:44 +0600

    remove applications_serializer

--- backend/hiking_crm/crm/serializers/admin/applications_serializer.py
+++
@@ -1,8 +0,0 @@
-from crm.models import ApplicationsForHike
-from rest_framework.serializers import ModelSerializer
-
-
-class ApplicationsForHikeSerializer(ModelSerializer):
-    class Meta:
-        model = ApplicationsForHike
-        fields = '__all__'
